114dfc8ef6ed472da41f9361c8334ca0de19ce9a
RarceD <akimbo170@gmail.com>
2020-03-22 08:40:55 +0100

    design the movements of the chracter

--- main_game.py
+++ main_game.py
@@ -2,23 +2,51 @@ import pygame
 import time
 pygame.init()
 
-win = pygame.display.set_mode((700, 500))
+win = pygame.display.set_mode((700, 480))
 win_x = 700
 win_y = 500
 
 pygame.display.set_caption("My fucking game")
 
-x = 50
-y = 50
-width = 40
-height = 60
+# Create a list of the pictures in Game Folder
+walkRight = [pygame.image.load('Game/R1.png'), pygame.image.load('Game/R2.png'), pygame.image.load('Game/R3.png'), pygame.image.load('Game/R4.png'), pygame.image.load('Game/R5.png'), pygame.image.load('Game/R6.png'), pygame.image.load('Game/R7.png'), pygame.image.load('Game/R8.png'), pygame.image.load('Game/R9.png')]
+walkLeft = [pygame.image.load('Game/L1.png'), pygame.image.load('Game/L2.png'), pygame.image.load('Game/L3.png'), pygame.image.load('Game/L4.png'), pygame.image.load('Game/L5.png'), pygame.image.load('Game/L6.png'), pygame.image.load('Game/L7.png'), pygame.image.load('Game/L8.png'), pygame.image.load('Game/L9.png')]
+bg = pygame.image.load('Game/bg.jpg')
+char = pygame.image.load('Game/standing.png')
+
+clock = pygame.time.Clock() # Don't have any idea about it
+height = 64
+width = 64
+x = 50 # variables for the caracter
+y = win_y - height - 30 
 velocity = 30
 
-isJump = False
+isJump = False # Variables for jumpping
 jumpCount = 10
+
+left = False
+right = False
+
+def reddrawGameWindow():
+    global walkCount
+    win.blit(bg,(0,0))
+    if (walkCount + 1 >= 27):
+        walkCount = 0
+    if (left):
+        win.blit(walkLeft[walkCount//3], (x,y))
+        walkCount += 1
+    elif (right):
+        win.blit(walkRight[walkCount//3], (x,y))
+        walkCount += 1
+    else:
+        win.blit(char, (x,y))
+
+
+    pygame.display.update()  # update the screen frames
+
 run = True
 while(run):
-    pygame.time.delay(50)
+    pygame.time.delay(50) ##64x64 images
     # Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -26,13 +54,17 @@ while(run):
     keys = pygame.key.get_pressed()  # check the diferent letters of the key board
     if (keys[pygame.K_RIGHT] and x < win_x - width - velocity):
         x += velocity
-    if (keys[pygame.K_LEFT]and x > velocity):
+        right = True
+        left = False
+    elif (keys[pygame.K_LEFT]and x > velocity):
         x -= velocity
+        left = True
+        right = False
+    else:
+        left = False #This variables are for the movement animations
+        right = False
+        walkCount = 0
     if not (isJump):
-        if (keys[pygame.K_UP] and y > velocity):
-            y -= velocity
-        if (keys[pygame.K_DOWN] and y < win_y - height - velocity):
-            y += velocity
         if (keys[pygame.K_SPACE]):
             isJump = True
     else:
@@ -45,8 +77,6 @@ while(run):
         else:
             jumpCount = 10
             isJump = False
+    reddrawGameWindow() #For drawing
 
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (250, 123, 11), (x, y, width, height)
-                     )  # draw a rectangule
-    pygame.display.update()  # update the screen frames
+pygame.quit()
