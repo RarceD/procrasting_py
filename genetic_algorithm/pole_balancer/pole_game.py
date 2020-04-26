4e1efc58b79244db290b33011df388eccbd97ab7
RarceD <akimbo170@gmail.com>
2020-04-26 09:38:07 +0200

    finish the climbing game

--- genetic_algorithm/pole_balancer/pole_game.py
+++ genetic_algorithm/pole_balancer/pole_game.py
@@ -2,128 +2,129 @@ import pygame
 import time
 import math
 
-WIN_HEIGHT = 400
-WIN_WIDTH = 600
+WIN_HEIGHT = 600
+WIN_WIDTH = 400
 pygame.font.init()
-FONT = pygame.font.SysFont("comicsans", 40)
+FONT = pygame.font.SysFont("comicsans", 30)
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Try to Balance")
 
-GRAVITY = 9.8
 
-
-class Pole:
-    def __init__(self, x, h):
+class Platform:
+    def __init__(self, x, y):
         self.x = x
-        self.y = WIN_HEIGHT - 50
-        self.pole_height = h
+        self.y = y
         self.lenght = 80
-        self.width = 50
-        self.vel = 5
-        self.eq = Equilibrium(self.x + self.lenght / 2,
-                              self.y, self.pole_height)
-        self.m = 2
-        self.aceleration =0 
+        self.width = 30
 
     def draw(self, win):
         pygame.draw.rect(win, (0, 0, 0),
                          (self.x, self.y, self.lenght, self.width))
         pygame.draw.rect(win, (24, 111, 217),
                          (self.x+5, self.y+5, self.lenght-10, self.width-10))
-        self.eq.draw(win, self.x + self.lenght / 2)
-    def move(self, time):
-        
-
-
-class Equilibrium():
-    def __init__(self, x, y, h):
-        self.x = x
-        self.y = y
-        self.pole_height = h
-        self.width = 20
-
-    def draw(self, win, x):
-        off = 5
-        pygame.draw.rect(win, (19, 8, 51),
-                         (x-10, WIN_HEIGHT - self.pole_height - 25, self.width, self.pole_height))
-        pygame.draw.rect(win, (174, 235, 52),
-                         (x-10 + off, WIN_HEIGHT - self.pole_height - 25 + off, self.width - 2*off, self.pole_height - 2*off))
 
 
 class Ball:
-    def __init__(self, x, y, t):
+    def __init__(self, x, y):
         self.x = x
-        self.y_0 = y  # the initial position
-        self.v_0 = 0  # the initial position
-        self.y = 0  # current position
-        self.v = 0  # the final velocity
-        self.start_time = t
-        self.m = 2
+        self.y = y  # current position
+        self.vel = 8
+        self.jump_counter = 20
+        self.num = 1
+        self.direction = 1
 
-    def move(self, time):
-        t = time - self.start_time
-        self.v = GRAVITY * t
-        self.y = self.y_0 + self.v_0*t + 0.5*GRAVITY*t*t
-        self.y_0 = self.y
-        self.v_0 = self.v
+    def move(self, platforms):
+        self.y += self.vel * self.direction
+        self.jump_counter -= 1
+        # If is in the bottom of the screen it jumps and reestart
+        if (self.y > WIN_HEIGHT-20 or self.colision(platforms)):
+            self.direction = -1
+            self.jump_counter = 20
+        if (self.jump_counter < 0):  # If is on the
+            self.direction = 1
+            self.jump_counter = 20
+        if (self.y < 0):
+            self.y = WIN_HEIGHT
 
-        if (self.y > 380):
-            self.y = 380
-            self.v = 0
+
+    def colision(self, platforms):
+        for platform in platforms:
+            if (self.x > platform.x and self.x < platform.x+platform.lenght):
+                if (self.y > platform.y-platform.width and self.y < platform.y+50):
+                    return True
+        return False
 
     def draw(self, win):
         pygame.draw.circle(win, (0, 0, 0), (int(self.x), int(self.y)), 20)
-        pygame.draw.circle(win, (150, 17, 133), (int(self.x), int(self.y)), 16)
+        pygame.draw.circle(win, (150, 17, 133), (int(self.x), int(self.y)), 15)
+
+    def move_right(self):
+        if (self.x < WIN_WIDTH-20):
+            self.x += self.vel
+
+    def move_left(self):
+        if (self.x > 20):
+            self.x += -self.vel
 
 
-
-        
-
-def draw_all(win, pole, time, balls):
+def draw_all(win,  balls, platforms, time, max_height):
     win.fill([105, 167, 209])
     text = FONT.render("Pole Play - RarceD", 1, (11, 0, 107))
-    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 6))
-    text_gen = FONT.render("Time: " + str(time) + "s", 1, (221, 0, 107))
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+    text_time = FONT.render("Time: " + str(time) + "s", 1, (221, 0, 107))
+    win.blit(text_time, (10, 6))
+    text_height = FONT.render(
+        str(WIN_HEIGHT-max_height) + " m", 1, (11, 0, 107))
+    win.blit(text_height, (10, 25))
+
     for ball in balls:
         ball.draw(win)
-    win.blit(text_gen, (10, 6))
-    pole.draw(win)
+    for platform in platforms:
+        platform.draw(win)
     pygame.display.update()
 
 
-def manual_movement(keys, pole):
-    if (keys[pygame.K_UP]):
-        pass
-    if (keys[pygame.K_RIGHT] and pole.x < WIN_WIDTH):
-        pole.x += pole.vel
-    if (keys[pygame.K_LEFT]and pole.x > 0):
-        pole.x -= pole.vel
+def manual_movement(keys, ball):
+    if (keys[pygame.K_RIGHT]):
+        ball.move_right()
+    if (keys[pygame.K_LEFT]):
+        ball.move_left()
 
 
-run = True
-pole = Pole(200, 100)
-# I start the game to train the generations:
-clock = pygame.time.Clock()
-# If they spend to much time I kill them
-start_time = time.time()
-balls = []
-while (run):
-    clock.tick(30)  # Every second it run 30 times
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            pygame.quit()
-            quit()
-            break
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            (x, y) = pygame.mouse.get_pos()
-            t = time.time()
-            balls.append(Ball(x, y, t))
-    if len(balls) > 0:
-        t = time.time()
-        for ball in balls:
-            ball.move(t)
+def main():
+    max_height = WIN_HEIGHT
+    run = True
+    plat_pos_y = WIN_HEIGHT/3
+    platforms = []
+    platforms.append(Platform(50, plat_pos_y))
+    platforms.append(Platform(WIN_WIDTH/2+50, plat_pos_y * 2-100))
+    platforms.append(Platform(50, plat_pos_y * 3 - 100))
 
-    keys = pygame.key.get_pressed()
-    manual_movement(keys, pole)
-    draw_all(WIN, pole, ' %.2f' % (time.time() - start_time), balls)
+    # I start the game to train the generations:
+    clock = pygame.time.Clock()
+    # If they spend to much time I kill them
+    start_time = time.time()
+    balls = []
+    balls.append(Ball(WIN_WIDTH-50, WIN_HEIGHT - 50))
+    ball = balls[0]
+    while (run):
+        clock.tick(30)  # Every second it run 30 times
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+                break
+
+        ball.move(platforms)
+        if (max_height > ball.y):
+            max_height = ball.y
+
+        keys = pygame.key.get_pressed()
+        manual_movement(keys, ball)
+
+        draw_all(WIN, balls, platforms, ' %.2f' %
+                 (time.time() - start_time), max_height)
+
+
+main()
