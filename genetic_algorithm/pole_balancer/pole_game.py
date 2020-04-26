79d1bf2417c2c70dbf9760ce8f7ddf2a35893488
RarceD <akimbo170@gmail.com>
2020-04-26 16:55:38 +0200

    cosa en paralelo 1

--- genetic_algorithm/pole_balancer/pole_game.py
+++ genetic_algorithm/pole_balancer/pole_game.py
@@ -212,3 +212,4 @@ if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-feedforward.txt')
     run(config_path)
+    print("HOLA")
