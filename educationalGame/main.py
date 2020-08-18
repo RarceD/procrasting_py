9514215264731f57c1c18b99e5b12b555fc37bb7
RarceD <akimbo170@gmail.com>
2020-08-18 21:14:41 +0200

    finish the color part and set the fix adjustment

---
+++ educationalGame/main.py
@@ -0,0 +1,9 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
+arcade.set_background_color(arcade.color.DARK_BLUE)
+arcade.start_render()
